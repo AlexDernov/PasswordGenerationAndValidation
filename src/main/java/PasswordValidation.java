46148164b32c41fb2ba8a4f9ec7702f1b1f3877c
Alexandra Dernova <alexder@gmx.net>
2023-12-04 15:18:32 +0100

    Add Method: numbersCheck

--- src/main/java/PasswordValidation.java
+++ src/main/java/PasswordValidation.java
@@ -1,5 +1,20 @@
 public class PasswordValidation {
+    public static boolean hasNumbers = false;
+    public static String numbers = "1234567890";
     public static boolean lengthCheck(String password) {
         return password.length() >= 8;
+    }
+    public static boolean numbersCheck(String password) {
+        char[] passwordChar = password.toCharArray();
+        int counter = 0;
+        for (char c : passwordChar) {
+            if (numbers.contains(String.valueOf(c))) {
+                counter++;
+            }
+            if (counter > 0) {
+                hasNumbers = true;
+            }
+        }
+        return hasNumbers;
     }
 }
